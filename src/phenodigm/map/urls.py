7e53555f63efea825beb97e670f288e0e285f047
Kim, GiBeom <gibum1228@gmail.com>
2022-08-20 18:00:46 +0900

    맵 클릭시 페이지 이동

--- src/phenodigm/map/urls.py
+++ src/phenodigm/map/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('analysis/', views.analysis, name='analysis')
+    # path('analysis/<name>/', views.analysis, name='analysis') # name은 동적인 값이 들어감
 ]
