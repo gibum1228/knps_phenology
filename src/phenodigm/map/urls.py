424a17fe4f6d47270b670fff32f8074660743482
Kim, GiBeom <gibum1228@gmail.com>
2022-08-21 15:24:20 +0900

    fusionCharts를 이용해 분석 그래프 그리기

--- src/phenodigm/map/urls.py
+++ src/phenodigm/map/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('analysis/', views.analysis, name='analysis')
+    path('analysis/', views.analysis, name='analysis'),
     # path('analysis/<name>/', views.analysis, name='analysis') # name은 동적인 값이 들어감
 ]
--- src/phenodigm/map/views.py
+++ src/phenodigm/map/views.py
@@ -6,6 +6,16 @@ from django.shortcuts import render
 from folium import plugins
 import folium
 import base64
+from .fusioncharts import FusionCharts
+from .fusioncharts import FusionTable
+from .fusioncharts import TimeSeries
+import json
+import requests
+import pandas as pd
+
+
+# 전역변수
+root = "/Users/beom/Desktop/git/data/knps/dl_data/"
 
 
 @xframe_options_exempt
@@ -29,7 +39,7 @@ def index(request):
     for i in range(len(name)):
         # 이미지 가져오기
         pic = base64.b64encode(open(f'../resource/{name[i]}.png', 'rb').read()).decode()
-        image_tag = f'<div style="text-align:center; "><a href="http://127.0.0.1:8000/analysis/?mountain={name[i]}&start_year=2003&end_year=2003&class_num=0&curve_fit=1&shape=1" target="_top"><img src="data:image/png;base64,{pic}" width="200" height="150"></a></div>'
+        image_tag = f'<div style="text-align:center; "><a href="http://127.0.0.1:8000/analysis/?knps={name[i]}&start_year=2003&end_year=2003&class_num=0&curve_fit=1&shape=1" target="_top"><img src="data:image/png;base64,{pic}" width="200" height="150"></a></div>'
         # iframe 생성
         iframe = folium.IFrame(image_tag, width=220, height=170)
         # html 띄울 popup 객체 생성
@@ -48,16 +58,68 @@ def index(request):
 
 
 def analysis(request):
-    property_list = ["mountain", "curve_fit", "start_year", "end_year", "class_num", "threshold", "shape"]
+    property_list = ["knps", "curve_fit", "start_year", "end_year", "class_num", "threshold", "shape"]
 
     db = {}
 
     if request.method == 'GET':
         for key in property_list:
             db[f"{key}"] = request.GET[f"{key}"] if request.GET.get(f"{key}") else ""
-    print(request.GET)
+
+    db['graph'] = get_chart(db['knps'], db['class_num'])
+
     return render(request, 'map/analysis.html', db)
 
 
 def predict(request):
     pass
+
+
+def get_Feb_day(year):
+    if year % 4 == 0 or year % 100 == 0 or year % 400 == 0:
+        day = 29
+    else:
+        day = 28
+
+    return day
+
+
+def get_chart(knps, class_num):
+    df = pd.read_csv(root + "bukhan/bukhan_2.csv") # curve fitting된 데이터 가져오기
+    data = [] # 그래프를 그리기 위한 데이터
+    schema = [{"name": "Time", "type": "date", "format": "%Y-%m-%d"}, {"name": "EVI", "type": "number"}]
+    info_day = [None, 31, None, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 월별 일 수 정보
+    year, month, day = 2003, 1, 1 # 년월일을 알기 위한 변수
+
+    for doy in df.iloc[:, 0].to_list(): # 첫번째 열 정보 가져오기(day 정보)
+        data.append([f"{year}-{month}-{day}", df.iloc[len(data), 1]])
+
+        day += 8
+        if month == 2:
+            day_limit = get_Feb_day(year)
+        else:
+            day_limit = info_day[month]
+
+        if day > day_limit:
+            day -= day_limit
+            month += 1
+
+            if month > 12:
+                year += 1
+                month = 1
+                day = 1
+
+    fusionTable = FusionTable(json.dumps(schema), json.dumps(data))
+    timeSeries = TimeSeries(fusionTable)
+
+    timeSeries.AddAttribute('chart', '{}')
+    timeSeries.AddAttribute('caption', '{"text":"식생지수 분석"}')
+    timeSeries.AddAttribute('subcaption', '{"text":"국립공원공단 레인저스"}')
+    timeSeries.AddAttribute('yaxis',
+                            '[{"plot":{"value":"EVI"},"format":{"prefix":""},"title":"EVI"}]')
+
+    # Create an object for the chart using the FusionCharts class constructor
+    fcChart = FusionCharts("timeseries", "ex1", 700, 450, "chart-1", "json", timeSeries)
+    print(len(df), len(data))
+    # returning complete JavaScript and HTML code, which is used to generate chart in the browsers.
+    return  fcChart.render()
