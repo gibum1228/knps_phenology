712f675857b64e5815208f1d780d339846975643
Kim, GiBeom <gibum1228@gmail.com>
2022-08-25 13:36:16 +0900

    페노캠 이미지 받고 정보 저장하는 알고리즘

--- src/phenodigm/map/views.py
+++ src/phenodigm/map/views.py
@@ -1,3 +1,5 @@
+import io
+from PIL import Image
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
@@ -11,23 +13,25 @@ from .fusioncharts import FusionTable
 from .fusioncharts import TimeSeries
 import json
 import pandas as pd
+import sys
+sys.path.append("/Users/beom/Desktop/git/knps_phenology/src/phenoKOR")
+import cv2 as cv
+import numpy as np
+import data_preprocessing as dp
+import matplotlib.pyplot as plt
 
 
 # 전역변수
-root = "/Users/ho/Desktop/dl/"
+root, middle = dp.get_info()
 
-'''홈 페이지'''
+
+# 홈 페이지
 @xframe_options_exempt # iframe 허용하기 위한 태그
 def index(request):
     m = folium.Map(location=[36.684273, 128.068635], zoom_start=6.5, width="100%", height="100%")  # 기본이 되는 지도 정보 가져오기
 
-    name = ["gaya", "gyeongju", "gyeryong", "naejang", "dadohae", "deogyu", "mudeung", "byeonsan", "bukhan", "seorak", # 산 이름
-            "sobaek", "songni", "odae", "worak", "wolchul", "juwang", "jiri", "chiak", "taebaek", "taean", "halla", "hallyeo"]
-    position = [(35.779385, 128.122559), (35.867430, 129.222565), (36.356057, 127.212067), (35.483333, 126.883333), # 국립공원명과 대치되는 (위도, 경도)
-                (34.708203, 125.901489), (35.824494, 127.787476), (35.134127, 126.988756), (35.680893, 126.531392), (37.70338, 127.032166),
-                (38.1573652, 128.4355274), (36.909725, 128.459374), (36.533333, 127.9), (37.724030, 128.599777), (36.852005, 128.197261),
-                (34.757310, 126.680823), (36.402218, 129.187889), (35.333333, 127.716667), (37.37169, 128.050509), (37.0545, 128.916666),
-                (36.78712, 126.143475), (33.366667, 126.533333), (34.75882, 127.97596)]
+    name = dp.get_knps_name_EN()
+    position = dp.get_knps_position()
 
     # 국립공원 수만큼 반복
     for i in range(len(name)):
@@ -52,7 +56,7 @@ def index(request):
     return render(request, 'map/index.html', {'map': maps}) # index.html에 map 변수에 maps 값 전달하기
 
 
-'''분석 페이지'''
+# 분석 페이지
 def analysis(request):
     property_list = ["knps", "curve_fit", "start_year", "end_year", "class_num", "threshold", "shape"] # GET 메소드로 주고 받을 변수 이름들
     db = {} # 데이터를 저장하고 페이지에 넘겨 줄 딕셔너리
@@ -68,7 +72,7 @@ def analysis(request):
     return render(request, 'map/analysis.html', db) # 웹 페이지에 값들 뿌려주기
 
 
-'''예측 페이지'''
+# 예측 페이지
 def predict(request):
     property_list = ["mountain", "curve_fit", "start_year", "end_year", "class_num", "threshold"]
 
@@ -77,24 +81,50 @@ def predict(request):
     if request.method == 'GET':
         for key in property_list:
             db[f"{key}"] = request.GET[f"{key}"] if request.GET.get(f"{key}") else ""
-    print(request.GET)
-    return  render(request, 'map/predict.html', db)
+
+    return render(request, 'map/predict.html', db)
 
 
-'''윤년 구하는 메소드'''
-def get_Feb_day(year):
-    # 4, 100, 400으로 나누어 떨어진다면 윤년
-    if year % 4 == 0 or year % 100 == 0 or year % 400 == 0:
-        day = 29
-    else:
-        day = 28
+# 페노캠 이미지 분석하는 페이지
+def phenocam(request):
+    db = {}
 
-    return day
+    if request.method == 'POST' and request.FILES['folder']:
+        image = dict(request.FILES)['folder'] # 폴더 내에 있는 모든 파일 가져오기
+        images = image[0]
+
+        print("===== images name =====")
+        # print('myfile read:', images.read())  # file 읽기
+        print('myfile size:', images.size)  # file 읽기
+        print('myfile content_type:', images.content_type)
+        print('myfile open:', images.open())
+        myfile_read = images.read()
+        print('myfile read type:', type(myfile_read))
+
+        data_io = io.BytesIO(myfile_read)
+        img_pil = Image.open(data_io)
+        print(img_pil)
+        numpy_image = np.array(img_pil)
+        open_cv_image = cv.cvtColor(numpy_image, cv.COLOR_RGB2BGR)
+        print(open_cv_image.shape)
+
+        print("=====")
+        print(len(image))
+
+        # encoding_img = np.fromstring(str(myfile_read), dtype=np.uint8)
+        # print(encoding_img)
+        # print(len(encoding_img))
+        # print(type(encoding_img))
+        # img = cv.imdecode(encoding_img, cv.IMREAD_COLOR)
+        # print(img)
+
+    return render(request, 'map/phenocam.html', db)
 
 
-'''연속된 그래프를 그려주는 메소드'''
+# 연속된 그래프를 그려주는 메소드
 def get_chart(ori_db):
-    df = pd.read_csv(root + "bukhan/bukhan_2.csv") # curve fitting된 데이터 가져오기
+    df = pd.read_csv(root + f"data{middle}knps_final.csv") # curve fitting된 데이터 가져오기
+
     data = [] # 그래프를 그리기 위한 데이터
     schema = [{"name": "Time", "type": "date", "format": "%Y-%m-%d"}, {"name": "EVI", "type": "number"}] # 하나의 data 구조
     info_day = [None, 31, None, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 월별 일 수 정보
@@ -105,7 +135,7 @@ def get_chart(ori_db):
 
         day += 8 # 8일 간격씩 데이터 추가
         if month == 2: # 2월은 윤년 여부 판단하기
-            day_limit = get_Feb_day(year)
+            day_limit = dp.get_Feb_day(year)
         else: # 2월이 아니면 해당 월의 일 수 가져오기
             day_limit = info_day[month]
 
@@ -137,7 +167,7 @@ def get_chart(ori_db):
     return  fcChart.render()
 
 
-'''연도별 그래프를 그려주는 메소드'''
+# 연도별 그래프를 그려주는 메소드
 def get_multi_plot(ori_db):
     df = pd.read_csv(root + "bukhan/bukhan_2.csv")  # curve fitting된 데이터 가져오기
 
